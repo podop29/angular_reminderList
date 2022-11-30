2b1e0525faf9835edaaf2d58193bc803b90d5870
stevan <stevangrubac123@gmail.com>
2022-11-30 13:03:05 -0500

    commit

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+  @Input() text: any;
+  @Input() color: any;
+  @Output() btnClick = new EventEmitter;
+
+  constructor() {}
+
+
+  onClick() {
+    this.btnClick.emit();
+  }
+
+
+
+
+
+
+}
